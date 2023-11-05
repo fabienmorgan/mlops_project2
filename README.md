bd70e3e058dae9a30891ac6d4438be5fd06f8388
fabienmorgan <fabien.morgan@criptext.com>
2023-11-05 14:58:35 +0100

    Build Docker images hinzugefügt, readme erweitert und gitignore hinzugefügt

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # MLOPS project 2
 
 This repository completes the fourth task in the second project of the subject MLOPS at HSLU in semester HS23. The repository contains all the project files including the Dockerfiles and describes all necessary steps to run the docker file on your own devices.
+
+## Project Description
+
+The goal of this project is to run a training of a predefined model in a Docker container. The code without any logging or optimised parameters was given to us(). The task of the first project was to optimize the hyperparameters. In this project we needed to use these optimized hyperparameters and log the training results with a library of our choice.
+
+## Build Docker images
+
+## Copy Docker images
---
+++ build_docker_images/distilBERT.py
@@ -0,0 +1,315 @@
+import sys
+import getopt
+import resource
+import pandas as pd
+
+from datetime import datetime
+from typing import Optional
+
+import wandb
+
+import datasets
+import torch
+from torch.utils.data import DataLoader
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup,
+)
+from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
+from pytorch_lightning.loggers import WandbLogger
+
+
+class GLUEDataModule(LightningDataModule):
+    task_text_field_map = {
+        "cola": ["sentence"],
+        "sst2": ["sentence"],
+        "mrpc": ["sentence1", "sentence2"],
+        "qqp": ["question1", "question2"],
+        "stsb": ["sentence1", "sentence2"],
+        "mnli": ["premise", "hypothesis"],
+        "qnli": ["question", "sentence"],
+        "rte": ["sentence1", "sentence2"],
+        "wnli": ["sentence1", "sentence2"],
+        "ax": ["premise", "hypothesis"],
+    }
+
+    glue_task_num_labels = {
+        "cola": 2,
+        "sst2": 2,
+        "mrpc": 2,
+        "qqp": 2,
+        "stsb": 1,
+        "mnli": 3,
+        "qnli": 2,
+        "rte": 2,
+        "wnli": 2,
+        "ax": 3,
+    }
+
+    loader_columns = [
+        "datasets_idx",
+        "input_ids",
+        "token_type_ids",
+        "attention_mask",
+        "start_positions",
+        "end_positions",
+        "labels",
+    ]
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        task_name: str = "mrpc",
+        max_seq_length: int = 128,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        **kwargs,
+    ):
+        super().__init__()
+        self.model_name_or_path = model_name_or_path
+        self.task_name = task_name
+        self.max_seq_length = max_seq_length
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+
+        self.text_fields = self.task_text_field_map[task_name]
+        self.num_labels = self.glue_task_num_labels[task_name]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def setup(self, stage: str):
+        self.dataset = datasets.load_dataset("glue", self.task_name)
+
+        for split in self.dataset.keys():
+            self.dataset[split] = self.dataset[split].map(
+                self.convert_to_features,
+                batched=True,
+                remove_columns=["label"],
+            )
+            self.columns = [c for c in self.dataset[split].column_names if c in self.loader_columns]
+            self.dataset[split].set_format(type="torch", columns=self.columns)
+
+        self.eval_splits = [x for x in self.dataset.keys() if "validation" in x]
+
+    def prepare_data(self):
+        datasets.load_dataset("glue", self.task_name)
+        AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset["train"], batch_size=self.train_batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["validation"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def test_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["test"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def convert_to_features(self, example_batch, indices=None):
+        # Either encode single sentence or sentence pairs
+        if len(self.text_fields) > 1:
+            texts_or_text_pairs = list(zip(example_batch[self.text_fields[0]], example_batch[self.text_fields[1]]))
+        else:
+            texts_or_text_pairs = example_batch[self.text_fields[0]]
+
+        # Tokenize the text/text pairs
+        features = self.tokenizer.batch_encode_plus(
+            texts_or_text_pairs, max_length=self.max_seq_length, pad_to_max_length=True, truncation=True
+        )
+
+        # Rename label to labels to make it easier to pass to model forward
+        features["labels"] = example_batch["label"]
+
+        return features
+
+class GLUETransformer(LightningModule):
+    def __init__(
+        self,
+        model_name_or_path: str,
+        num_labels: int,
+        task_name: str,
+        learning_rate: float = 2e-5,
+        adam_epsilon: float = 1e-8,
+        warmup_steps: int = 0,
+        weight_decay: float = 0.0,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        eval_splits: Optional[list] = None,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.config = AutoConfig.from_pretrained(model_name_or_path, num_labels=num_labels)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, config=self.config)
+        self.metric = datasets.load_metric(
+            "glue", self.hparams.task_name, experiment_id=datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        )
+        self.save_hyperparameters()
+
+    def forward(self, **inputs):
+        return self.model(**inputs)
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = outputs[0]
+        self.log("train_loss", loss)
+        print(memory_usage())
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        outputs = self(**batch)
+        val_loss, logits = outputs[:2]
+
+        if self.hparams.num_labels > 1:
+            preds = torch.argmax(logits, axis=1)
+        elif self.hparams.num_labels == 1:
+            preds = logits.squeeze()
+
+        labels = batch["labels"]
+
+        return {"loss": val_loss, "preds": preds, "labels": labels}
+
+    def validation_epoch_end(self, outputs):
+        if self.hparams.task_name == "mnli":
+            for i, output in enumerate(outputs):
+                # matched or mismatched
+                split = self.hparams.eval_splits[i].split("_")[-1]
+                preds = torch.cat([x["preds"] for x in output]).detach().cpu().numpy()
+                labels = torch.cat([x["labels"] for x in output]).detach().cpu().numpy()
+                loss = torch.stack([x["loss"] for x in output]).mean()
+                self.log(f"val_loss_{split}", loss, prog_bar=True)
+                split_metrics = {
+                    f"{k}_{split}": v for k, v in self.metric.compute(predictions=preds, references=labels).items()
+                }
+                self.log_dict(split_metrics, prog_bar=True)
+            return loss
+
+        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
+        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
+        loss = torch.stack([x["loss"] for x in outputs]).mean()
+        self.log("val_loss", loss, prog_bar=True)
+        self.log_dict(self.metric.compute(predictions=preds, references=labels), prog_bar=True)
+
+    def configure_optimizers(self):
+        """Prepare optimizer and schedule (linear warmup and decay)"""
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        return [optimizer], [scheduler]
+
+def load_arguments(argv, file_path, learning_rate, epsilon, warmup_steps):
+  arg_help = "{0} -f <file_path> -l <learning_rate> -e <epsilon> -w <warmup_steps>".format(argv[0])
+    
+  try:
+    opts, args = getopt.getopt(argv[1:], "hf:l:e:w:", ["help", "file_path=", 
+      "learning_rate=", "epsilon=", "warmup_steps="])
+  except:
+      print(arg_help)
+      sys.exit(2)
+    
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      print(arg_help)  # print the help message
+      sys.exit(2)
+    elif opt in ("-f", "--file_path"):
+      file_path = arg
+    elif opt in ("-l", "--learning_rate"):
+      print(f'l: {arg}')
+      learning_rate = float(arg)
+    elif opt in ("-e", "--epsilon"):
+      print(f'e: {arg}')
+      epsilon = float(arg)
+    elif opt in ("-w", "--warmup_steps"):
+      print(f'w: {arg}')
+      warmup_steps = int(arg)
+
+  return file_path, learning_rate, epsilon, warmup_steps
+
+def obj_size_fmt(num):
+    if num<10**3:
+        return "{:.2f}{}".format(num,"B")
+    elif ((num>=10**3)&(num<10**6)):
+        return "{:.2f}{}".format(num/(1.024*10**3),"KB")
+    elif ((num>=10**6)&(num<10**9)):
+        return "{:.2f}{}".format(num/(1.024*10**6),"MB")
+    else:
+        return "{:.2f}{}".format(num/(1.024*10**9),"GB")
+
+def memory_usage():
+    memory_usage_by_variable=pd.DataFrame({k:sys.getsizeof(v)\
+    for (k,v) in globals().items()},index=['Size'])
+    memory_usage_by_variable=memory_usage_by_variable.T
+    memory_usage_by_variable=memory_usage_by_variable.sort_values(by='Size',ascending=False).head(10)
+    memory_usage_by_variable['Size']=memory_usage_by_variable['Size'].apply(lambda x: obj_size_fmt(x))
+    return memory_usage_by_variable
+
+if __name__ == "__main__":
+  seed_everything(42)
+
+  print(memory_usage())
+
+  wandb.init(entity="fabien-morgan", project="project-2")
+
+  learning_rate: float = 2e-5
+  epsilon: float = 1e-8
+  warmup_steps: int = 0
+  file_path = "model.pth"
+
+  file_path, learning_rate, epsilon, warmup_steps = load_arguments(sys.argv, file_path, learning_rate, epsilon, warmup_steps)
+
+  wandb_logger = WandbLogger(project='project-2', log_model="all")
+  
+  dm = GLUEDataModule(
+      model_name_or_path="distilbert-base-uncased",
+      task_name="mrpc",
+  )
+  dm.setup("fit")
+  model = GLUETransformer(
+      model_name_or_path="distilbert-base-uncased",
+      num_labels=dm.num_labels,
+      eval_splits=dm.eval_splits,
+      task_name=dm.task_name,
+      learning_rate=learning_rate,
+      adam_epsilon=epsilon,
+      warmup_steps=warmup_steps
+  )
+
+  trainer = Trainer(
+      max_epochs=3,
+      accelerator="auto",
+      devices=1 if torch.cuda.is_available() else None,
+      logger=wandb_logger
+  )
+
+  print(memory_usage())
+
+  trainer.fit(model, datamodule=dm)
+
+
+  torch.save(model.state_dict(), file_path)
+  wandb.finish()
